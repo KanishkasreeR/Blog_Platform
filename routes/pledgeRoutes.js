8fe173a0faf2edbb8945823d44d0259d74d2affd
R Kanishkasree <kanishkasree804@gmail.com>
2024-08-12 12:12:51 +0530

    basic setup

--- routes/pledgeRoutes.js
+++ routes/pledgeRoutes.js
@@ -5,9 +5,9 @@ const auth = require('../middlewares/auth')
 const router = express.Router();
 
 router.post('/createPledge', auth,pledgeController.createPledge);
-router.get('/getAllPledges',auth, pledgeController.getAllPledges);
+router.get('/getAllPledges', pledgeController.getAllPledges);
 router.get('/getPledgesByCampaignId/:campaignId',pledgeController.getPledgesByCampaignId);
 router.get('/getPledgesByBackerId/:backerId',pledgeController.getPledgesByBackerId);
-router.delete('/deletePledge/:pledgeId',pledgeController.deletePledge);
+router.delete('/deletePledge/:pledgeId',auth,pledgeController.deletePledge);
 
 module.exports = router;
